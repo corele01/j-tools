36fc6c7548f7b3300ed29b138abc024f96d9d607
刘君 <732007737@qq.com>
2020-05-09 13:30:13 +0800

    up

--- src/views/tools/JsonFormat.vue
+++ src/views/tools/JsonFormat.vue
@@ -3,15 +3,38 @@
       <a-card>
           This is jsonFormat Page.
       </a-card>
+      <div class="json-class">
+          <vue-json-editor :showBtns="false" lang="zh" v-model="josnEditor" mode="code" search="false" mainMenuBar="false"/>
+      </div>
+      
   </div>
 </template>
 
 <script>
+import VueJsonEditor from 'vue-json-editor'
 export default {
-    name:'JsonFormat'
+    name:'JsonFormat',
+    components:{
+        VueJsonEditor
+    },
+    data(){
+        return{
+            josnEditor:{
+                mode:'tree'
+            }
+        }    
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    .json-class{
+        width: 100%;
+        /deep/.jsoneditor-vue{
+            height:700px;
+        }
+        /deep/.json-save-btn{
+            cursor: pointer;
+        }
+    }
 </style>
